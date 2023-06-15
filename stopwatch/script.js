8041f3413027ed2b0ca27e9c9b517049522af0ad
Dinil Thilakarathne <sonacode44@gmail.com>
2023-06-15 08:36:23 +0530

    update stopwatch files

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,11 +1,11 @@
-let hours = 0;
 let minutes = 0;
 let seconds = 0;
+let miliseconds = 0;
 let timer;
 
 function startTimer() {
   // Start the timer by setting an interval that calls the updateTimer function every second
-  timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 10);
 }
 function stopTimer() {
     // Stop the timer by clearing the interval
@@ -13,12 +13,12 @@ function stopTimer() {
 }
 function resetTimer() {
     // Reset the timer by changing values
-    hours = 0;
     minutes = 0;
     seconds = 0;
-    document.getElementById("hours").textContent = padTime(hours);
+    miliseconds = 0;
     document.getElementById("minutes").textContent = padTime(minutes);
     document.getElementById("seconds").textContent = padTime(seconds);
+    document.getElementById("miliseconds").textContent = padTime(miliseconds);
 
     // Stop the timer by clearing the interval
     clearInterval(timer);
@@ -26,22 +26,22 @@ function resetTimer() {
 
 function updateTimer() {
   // Increment the seconds
-  seconds++;
+  miliseconds++;
 
   // Check if seconds reach 60, if so, reset seconds and increment minutes
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
+  if (miliseconds === 100) {
+    miliseconds = 0;
+    seconds++;
 
-    if (minutes === 60) {
-      minutes = 0;
-      hours++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
     }
   }
   // Update the HTML elements with the updated time values
-  document.getElementById("hours").textContent = padTime(hours);
   document.getElementById("minutes").textContent = padTime(minutes);
   document.getElementById("seconds").textContent = padTime(seconds);
+  document.getElementById("miliseconds").textContent = padTime(miliseconds);
 }
 
 function padTime(time) {
