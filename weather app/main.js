06f4fe5d305193101eb419ed50527e049e03c3a3
Dinil Thilakarathne <sonacode44@gmail.com>
2023-09-10 16:32:58 +0530

    add weather app files

---
+++ weather app/main.js
@@ -0,0 +1,36 @@
+// OpenWeather api 
+const apiKey  = "" // add your api key
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
+
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+// fetch data from the API 
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+  var data = await response.json()
+
+  document.querySelector('.weather').style.display = "flex"
+
+  // display data 
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+  document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
+
+  // change weather icon 
+  if(data.weather[0].main == "Clouds"){
+    weatherIcon.src = "./public/clouds.png"
+  }else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "./public/rain.png"
+  }else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "./public/drizzle.png"
+  }else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = "./public/mist.png"
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+})
