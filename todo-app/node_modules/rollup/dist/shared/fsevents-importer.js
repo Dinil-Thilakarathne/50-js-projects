baed50368167879ce2fe3f9b05ef49c8b4786ca1
Dinil Thilakarathne <sonacode44@gmail.com>
2023-09-17 10:52:15 +0530

    add todo-app

---
+++ todo-app/node_modules/rollup/dist/shared/fsevents-importer.js
@@ -0,0 +1,37 @@
+/*
+  @license
+	Rollup.js v3.29.1
+	Sun, 10 Sep 2023 05:55:18 GMT - commit 40da6f85525c2ce48d9fd8d5c3579c41d8ef47a8
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+let fsEvents;
+let fsEventsImportError;
+async function loadFsEvents() {
+    try {
+        ({ default: fsEvents } = await import('fsevents'));
+    }
+    catch (error) {
+        fsEventsImportError = error;
+    }
+}
+// A call to this function will be injected into the chokidar code
+function getFsEvents() {
+    if (fsEventsImportError)
+        throw fsEventsImportError;
+    return fsEvents;
+}
+
+const fseventsImporter = /*#__PURE__*/Object.defineProperty({
+    __proto__: null,
+    getFsEvents,
+    loadFsEvents
+}, Symbol.toStringTag, { value: 'Module' });
+
+exports.fseventsImporter = fseventsImporter;
+exports.loadFsEvents = loadFsEvents;
+//# sourceMappingURL=fsevents-importer.js.map
